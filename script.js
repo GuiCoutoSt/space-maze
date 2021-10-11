aaf982401018c74e598ef92de9b36487b3bb49e0
Guilherme Couto <gcoutos95@gmail.com>
2021-10-11 18:52:46 -0300

    Adding sound effects / Eliminating game bugs / Adding a Play Again or exit options / Adding a victory popUp / Adding the victory animation / Adding animations & styles.

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-// Intro
-// reload: location.reload()
+// soundEffects
+const mainThemeMusic = document.createElement('audio');
+mainThemeMusic.src = './assets/sounds/8\ BIT\ Interstellar\ Main\ Theme\ Hans\ Zimmer\ 8\ BIT.mp3';
+mainThemeMusic.loop = 'true';
+mainThemeMusic.volume = 0.5;
+mainThemeMusic.playbackRate = 1.20;
+
+const victoryMusic = document.createElement('audio');
+victoryMusic.src = './assets/sounds/Interstellar\ main\ theme\ 8-bit.mp3';
+victoryMusic.loop = 'true';
+victoryMusic.volume = 0.6;
+
+const spaceshipLaunch = document.createElement('audio');
+spaceshipLaunch.src = './assets/sounds/spacechip-launch.mp3';
+spaceshipLaunch.currentTime = 0;
+spaceshipLaunch.playbackRate = 5;
+
+const spaceshipPassing = document.createElement('audio');
+spaceshipPassing.src = './assets/sounds/final.mp3';
+
+const spaceshipMoving = document.createElement('audio');
+spaceshipMoving.src = './assets/sounds/spaceship-moving.mp3';
+
+const bipSound = document.createElement('audio');
+bipSound.src = './assets/sounds/bip.mp3';
+// soundEffects
+
 const body = document.body;
 
 const headerCommands = document.getElementById('headerCommands');
@@ -18,7 +43,6 @@ const planetNine = document.createElement('img');
 planetNine.classList.add('section__planet-nine');
 planetNine.src = './assets/images/planet-nine.png';
 
-// Quando essa função for utilizada, a seção de instruções será montada
 const instructions = () => {
     header.style.marginBottom = '50px';
     generateSection.innerHTML = '';
@@ -33,7 +57,7 @@ const instructions = () => {
     instructionsList.classList.add('section__instructions-list');
 
     const instructionsArray = ['To move the spaceship, use the Arrow Keys on your keyboard.',
-                               'Your mission is to reach the Planet 9.'];
+                               'Your mission is to reach the Planet 9:'];
     let c = 1;
 
     instructionsArray.forEach( item => {
@@ -70,15 +94,15 @@ const instructions = () => {
     const startButton = document.getElementById('pressStart');
 
     startButton.addEventListener('click', () => {
+        spaceshipPassing.play();
+
         generateSection.innerHTML = '';
         header.innerHTML = '';
 
         body.classList.add('body__animation-game');
         body.classList.add('body-game-background');
-
-        generateMaze();
-
         
+        generateMaze();
     }); 
 }
 
@@ -94,6 +118,9 @@ const spaceship = document.getElementById('spaceship');
 const pressNext = document.getElementById('pressNext');
 
 pressNext.addEventListener('click', () => {
+    mainThemeMusic.play();
+    spaceshipLaunch.play();
+
     body.classList.remove('body__animation-init');
 
     spaceship.style.animation = 'spaceship-launcher 1s';
@@ -103,12 +130,22 @@ pressNext.addEventListener('click', () => {
     headerCommands.innerText = 'Press Start';
 });
 
+const disableKeyBoard = () => {
+    document.onkeydown = (e) => {
+        console.log(false)
+        return false;
+    }
+}
+
 const generateMaze = () => {
     spaceshipInside.classList.add('inside-spaceship-animation');
 
-    setTimeout(function() {
-        spaceshipInside.classList.remove('inside-spaceship-animation')
-    }, 5000); 
+    setTimeout(function () {
+        spaceshipInside.classList.remove('inside-spaceship-animation');
+
+        body.classList.remove('body__animation-game');
+
+    }, 5000);
 
     const map = [
         "WWWWWWWWWWWWWWWWWWWWW",
@@ -138,7 +175,7 @@ const generateMaze = () => {
         for (let k = 0; k < newMap[j].length; k++) {
             const houses = document.createElement('div');
             houses.id = `${j}-${k}`;
-            
+
             if (newMap[j][k] === 'W') {
                 houses.classList.add('maze-wall');
                 houses.setAttribute('data-type', 'W');
@@ -148,16 +185,16 @@ const generateMaze = () => {
             } else if (newMap[j][k] === ' ') {
                 houses.classList.add('maze-space');
                 houses.setAttribute('data-type', 'B'); // B for BLANK
-                
+
                 board.appendChild(houses);
 
             } else if (newMap[j][k] === 'S') {
-
                 spaceshipInside.classList.add('maze-spaceship');
                 houses.classList.add('maze-space');
                 houses.setAttribute('data-type', 'S');
 
                 houses.appendChild(spaceshipInside);
+
                 board.appendChild(houses);
             } else {
                 planetNine.classList.add('maze-planet-nine');
@@ -165,100 +202,233 @@ const generateMaze = () => {
                 houses.setAttribute('data-type', 'F');
                 houses.appendChild(planetNine);
                 board.appendChild(houses);
-            } 
-            
+            }
+
         }
-    }   
+    }
 
     generateSection.appendChild(board);
 
-    const player = {
+    let player = {
         j: 9,
         k: 0,
-        spaceship: document.getElementById('insideSpaceship')
+        spaceship: spaceshipInside
     }
+
+    disableKeyBoard();
+
+    setTimeout (function () {
+        document.onkeydown = (evt) => {
+            const key = evt.key;
     
-    document.addEventListener('keydown', (evt) => {
-        const key = evt.key;
-        
-        console.log(player.j, player.k)
-
-        if (key === 'ArrowRight') {
-            player.spaceship.src = './assets/images/spaceshipright.png';
-
-            player.spaceship.classList.add('inside-spaceship-slideRight');
-
-            setTimeout(function() {
-                player.spaceship.classList.remove('inside-spaceship-slideRight')
-            }, 100);
-
-            if (document.getElementById(`${player.j}-${player.k + 1}`).dataset.type === 'F') {
-                winningCondition(player.j, player.k, player.spaceship);
-            }
-
-            if (document.getElementById(`${player.j}-${player.k + 1}`).dataset.type === 'B') {
-                player.k++;
-
-                document.getElementById(`${player.j}-${player.k}`).appendChild(player.spaceship);
-            }
+            movingTheSpaceship(key, player);
         }
-
-        else if (key === 'ArrowLeft') {
-            player.spaceship.src = './assets/images/spaceshipleft.png';
-
-            player.spaceship.classList.add('inside-spaceship-slideLeft');
-
-            setTimeout(function() {
-                player.spaceship.classList.remove('inside-spaceship-slideLeft')
-            }, 100);
-
-            if (document.getElementById(`${player.j}-${player.k - 1}`).dataset.type === 'B') {
-                player.k--;
-
-                document.getElementById(`${player.j}-${player.k}`).appendChild(player.spaceship);
-            }
-        }
-
-        else if (key === 'ArrowUp') {
-            player.spaceship.src = './assets/images/spaceshipup.png';
-
-            player.spaceship.classList.add('inside-spaceship-slideTop');
-
-            setTimeout(function() {
-                player.spaceship.classList.remove('inside-spaceship-slideTop')
-            }, 100);
-
-            if (document.getElementById(`${player.j - 1}-${player.k}`).dataset.type === 'B') {
-                player.j--;
-
-                document.getElementById(`${player.j}-${player.k}`).appendChild(player.spaceship);
-            }
-        }
-
-        else if (key === 'ArrowDown') {
-            player.spaceship.src = './assets/images/spaceshipdown.png';
-
-            player.spaceship.classList.add('inside-spaceship-slideDown');
-
-            setTimeout(function() {
-                player.spaceship.classList.remove('inside-spaceship-slideDown')
-            }, 100);
-
-            if (document.getElementById(`${player.j + 1}-${player.k}`).dataset.type === 'B') {
-                player.j++;
-
-                document.getElementById(`${player.j}-${player.k}`).appendChild(player.spaceship);
-            }
-        }
-    });
+    }, 5000);
 }
 
-const winningCondition = (j, k, player) => {
-    if (j === 8 && k === 19) {
-        document.getElementById(`8-20`).removeChild(planetNine);
 
-        document.getElementById(`8-20`).appendChild(player);
+const movingTheSpaceship = (evt, player) => {
+
+    if (evt === 'ArrowRight') {
+        spaceshipMoving.play();
+
+        player.spaceship.src = './assets/images/spaceshipright.png';
+        
+        player.spaceship.classList.add('inside-spaceship-slideRight');
+
+        setTimeout(function () {
+            player.spaceship.classList.remove('inside-spaceship-slideRight')
+        }, 100);
+
+        if (document.getElementById(`${player.j}-${player.k + 1}`).id === '8-20') {
+            winningCondition(player.spaceship);
+        }
+
+        if (document.getElementById(`${player.j}-${player.k + 1}`).dataset.type === 'B') {
+            player.k++;
+
+            document.getElementById(`${player.j}-${player.k}`).appendChild(player.spaceship);
+        }
     }
+
+    else if (evt === 'ArrowLeft') {
+        spaceshipMoving.play();
+
+        player.spaceship.src = './assets/images/spaceshipleft.png';
+
+        player.spaceship.classList.add('inside-spaceship-slideLeft');
+
+        setTimeout(function () {
+            player.spaceship.classList.remove('inside-spaceship-slideLeft')
+        }, 100);
+
+        if (document.getElementById(`${player.j}-${player.k - 1}`).dataset.type === 'B') {
+            player.k--;
+
+            document.getElementById(`${player.j}-${player.k}`).appendChild(player.spaceship);
+        }
+    }
+
+    else if (evt === 'ArrowUp') {
+        spaceshipMoving.play();
+
+        player.spaceship.src = './assets/images/spaceshipup.png';
+
+        player.spaceship.classList.add('inside-spaceship-slideTop');
+
+        setTimeout(function () {
+            player.spaceship.classList.remove('inside-spaceship-slideTop')
+        }, 100);
+
+        if (document.getElementById(`${player.j - 1}-${player.k}`).dataset.type === 'B') {
+            player.j--;
+
+            document.getElementById(`${player.j}-${player.k}`).appendChild(player.spaceship);
+        }
+    }
+
+    else if (evt === 'ArrowDown') {
+        spaceshipMoving.play();
+
+        player.spaceship.src = './assets/images/spaceshipdown.png';
+
+        player.spaceship.classList.add('inside-spaceship-slideDown');
+
+        setTimeout(function () {
+            player.spaceship.classList.remove('inside-spaceship-slideDown')
+        }, 100);
+
+        if (document.getElementById(`${player.j + 1}-${player.k}`).dataset.type === 'B') {
+            player.j++;
+
+            document.getElementById(`${player.j}-${player.k}`).appendChild(player.spaceship);
+        }
+    }
+}
+
+const winningCondition = (spaceship) => {
+    disableKeyBoard();
+
+    mainThemeMusic.pause();
+
+    
+    spaceshipPassing.play();
+
+    victoryMusic.currentTime = 0;
+    victoryMusic.play();
+
+    const mazeEnd = document.getElementById('8-20');
+
+    mazeEnd.removeChild(planetNine);
+
+    mazeEnd.appendChild(spaceship);
+
+    setTimeout(victoryAnimation, 400);
+}
+
+const victoryAnimation = () => {
+    generateSection.innerHTML = '';
+
+    body.classList.add('body__end');
+    body.classList.remove('body-game-background');
+
+    const planetNine = document.createElement('img');
+    planetNine.src = './assets/images/planet-nine.png';
+    planetNine.classList.add('popup__planetNine');
+
+    const spaceship = document.createElement('img');
+    spaceship.src = './assets/images/spaceshipup.png';
+    spaceship.classList.add('spaceship-end-1');
+    
+    generateSection.appendChild(planetNine);
+    generateSection.appendChild(spaceship);
+
+    setTimeout(function () {
+        generateSection.innerHTML = '';
+
+        victoryPopUp();
+    }, 7500);
+}
+
+const victoryPopUp = () => {
+    const popupDiv = document.createElement('div');
+    popupDiv.classList.add('popup');
+    popupDiv.classList.add('popup__animation');
+
+    const popupTitle = document.createElement('h2');
+    popupTitle.innerText = 'CONGRATULATIONS!';
+    popupTitle.classList.add('popup__title');
+
+    const popupParagraphArray = [
+        'You made it young Cadet!',
+        'You\'ve accomplished your mission!',
+        'What do you want to do now?'
+    ];
+
+    const popupParagraphNest = document.createElement('div');
+
+    popupParagraphArray.forEach( item => {
+        const popupParagraph = document.createElement('p');
+        popupParagraph.innerText = item;
+        popupParagraph.classList.add('popup__paragraph');
+
+        popupParagraphNest.appendChild(popupParagraph);
+    });
+    
+
+    const popupImg = document.createElement('img');
+    popupImg.src = './assets/images/the-eye.png';
+    popupImg.classList = 'popup__img';
+
+    const buttonsNest = document.createElement('div');
+    buttonsNest.classList.add('buttons-nest');
+
+    const resetButton = document.createElement('button');
+    resetButton.id = 'resetButton';
+    resetButton.classList.add('popup__button');
+    resetButton.innerText = 'PLAY AGAIN';
+
+    const exitButton = document.createElement('button');
+    exitButton.id = 'exitButton';
+    exitButton.classList.add('popup__button--red');
+    exitButton.innerText = 'EXIT X';
+
+    buttonsNest.appendChild(resetButton);
+    buttonsNest.appendChild(exitButton);
+
+    popupDiv.appendChild(popupTitle);
+    popupDiv.appendChild(popupParagraphNest);
+    popupDiv.appendChild(popupImg);
+    popupDiv.appendChild(buttonsNest);
+
+    generateSection.appendChild(popupDiv);
+
+    document.getElementById('resetButton').addEventListener('click', () => {
+        generateSection.innerHTML = '';
+        board.innerHTML = '';
+
+        body.classList.remove('body__end');
+
+        body.classList.add('body__animation-game');
+        body.classList.add('body-game-background');
+
+        victoryMusic.pause();
+
+        spaceshipPassing.play();
+
+        mainThemeMusic.currentTime = 0;
+        mainThemeMusic.play();
+        
+        generateMaze();
+    });
+
+    document.getElementById('exitButton').addEventListener('click', () => {
+        bipSound.play();
+        setTimeout( function () {
+            location.reload();
+        }, 500);
+        
+    });
 }
 
 
